38740c30a5491045ab83305498e1e58c354ddcf9
Sajin Saji George <sajinsajigeorge@gmail.com>
2025-03-20 09:41:17 +0530

    completed

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const secretKey = 'V5u39J3ijbMGx6IGVjqLUwdWW4Pitt3iqOiC7jyLA10=';
+
 const encrypt = (payload) => {
-  // encrypt the payload and return token
-}
+  try {
+    
+    const token = jwt.sign(payload, secretKey, { algorithm: 'HS256', expiresIn: '1h' });
+    console.log('Encrypted Token:', token);  
+    return token;
+  } catch (error) {
+    console.error('Error during encryption:', error);
+    throw new Error('Failed to encrypt data');
+  }
+};
 
 const decrypt = (token) => {
-  // return decoded payload
-}
+  try {
+    
+    const decoded = jwt.verify(token, secretKey);
+    console.log('Decoded Payload:', decoded);  
+    return decoded;
+  } catch (error) {
+    console.error('Error during decryption:', error);
+    throw new Error('Failed to decrypt token');
+  }
+};
 
 module.exports = {
   encrypt,
